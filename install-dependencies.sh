9b351f13d5495a9b3ab745a0efeac37a7b4f9a80
disturbed-condor <disturbed-condor@tutamail.com>
2025-12-23 15:34:54 -0500

    last line of install-dependencies.sh to deactivate virtual environment

---
+++ install-dependencies.sh
@@ -0,0 +1,17 @@
+python3 -m venv venv
+. venv/bin/activate
+python3 -m pip install --upgrade pip pyglet
+cp -rn venv/lib/python3.10/site-packages/* episode-1/
+cp -rn venv/lib/python3.10/site-packages/* episode-2/
+cp -rn venv/lib/python3.10/site-packages/* episode-3/
+cp -rn venv/lib/python3.10/site-packages/* episode-4/
+cp -rn venv/lib/python3.10/site-packages/* episode-5/
+cp -rn venv/lib/python3.10/site-packages/* episode-6/
+cp -rn venv/lib/python3.10/site-packages/* episode-7/
+cp -rn venv/lib/python3.10/site-packages/* episode-8/
+cp -rn venv/lib/python3.10/site-packages/* episode-9/
+cp -rn venv/lib/python3.10/site-packages/* episode-10/
+python3 -m pip install --upgrade pip pyglet base36 nbtlib
+cp -rn venv/lib/python3.10/site-packages/* episode-11/
+cp -rn venv/lib/python3.10/site-packages/* episode-12/
+deactivate
